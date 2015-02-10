58a5dd08decc43e84ad339f83aa945064365a086
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-02-10 07:40:24 -0500

    refactored paper corpus building to use luigi

--- pipeline/repdocs.py
+++ pipeline/repdocs.py
@@ -1,102 +1,123 @@
+import os
 import sys
 import csv
 import logging
+
 import pandas as pd
 import gensim
+import luigi
+
 import doctovec
 import util
+import aminer
+import config
 
 
-def read_paper_repdocs(papers_file):
-    for record in util.yield_csv_records(papers_file):
-        repdoc = '%s %s' % (record[1], record[4])
-        yield (record[0], repdoc.decode('utf-8'))
+class BuildPaperRepdocs(luigi.Task):
+    """Build representative documents for each paper in the DBLP corpus."""
+
+    def requires(self):
+        return aminer.ParsePapersToCSV()
+
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(config.data_dir, 'repdoc-by-paper.csv'))
+
+    def read_paper_repdocs(self):
+        paper_file = self.input()[0]
+        for record in util.iter_csv_fwrapper(paper_file):
+            repdoc = '%s %s' % (record[1], record[4])
+            yield (record[0], repdoc.decode('utf-8'))
+
+    def run(self):
+        """The repdoc for a single paper consists of its title and abstract,
+        concatenated with space between. The paper records are read from a csv
+        file and written out as (paper_id, repdoc) pairs.
+        """
+        docs = self.read_paper_repdocs()
+        rows = ((docid, doc.encode('utf-8')) for docid, doc in docs)
+        util.write_csv_to_fwrapper(self.output(), ('paper_id', 'doc'), rows)
 
 
-def write_paper_repdocs(paper_file, outfile='repdoc-by-paper.csv'):
-    """The repdoc for a single paper consists of its title and abstract,
-    concatenated with space between. The paper records are read from a csv file
-    and written out as (paper_id, repdoc) pairs.
+class BuildPaperRepdocVectors(luigi.Task):
+    """Vectorize paper repdocs, preprocessing terms."""
 
-    :param str paper_file: csv paper records filename.
-    :param str outfile: csv paper repdoc output filename.
-    """
-    docs = read_paper_repdocs(paper_file)
-    with open(outfile, 'w') as doc_file:
-        doc_writer = csv.writer(doc_file)
-        doc_writer.writerow(('paper_id', 'doc'))
-        for docid, doc in docs:
-            doc_writer.writerow((docid, doc.encode('utf-8')))
+    def requires(self):
+        return BuildPaperRepdocs()
 
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(config.data_dir, 'repdoc-by-paper-vectors.csv'))
 
-def write_paper_repdoc_vectors(paper_file,
-                               outfile='repdoc-by-paper-vectors.csv'):
-    """The repdoc for a single paper consists of its title and abstract,
-    concatenated with space between. The paper records are read from a csv file
-    and written out as (paper_id, repdoc) pairs.
-
-    :param str paper_file: csv paper records filename.
-    :param str outfile: csv paper repdoc output filename.
-    """
-    docs = read_paper_repdocs(paper_file)
-    with open(outfile, 'w') as vec_file:
-        vec_writer = csv.writer(vec_file)
-        vec_writer.writerow(('paper_id', 'doc'))
-        for docid, doc in docs:
-            vector = doctovec.doctovec(doc)
-            concat = '|'.join(vector).encode('utf-8')
-            vec_writer.writerow((docid, concat))
+    def run(self):
+        repdocs = util.iter_csv_fwrapper(self.input())
+        docs = ((docid, doc.decode('utf-8')) for docid, doc in repdocs)
+        vecs = ((docid, doctovec.vectorize(doc)) for docid, doc in docs)
+        rows = ((docid, '|'.join(doc).encode('utf-8')) for docid, doc in vecs)
+        util.write_csv_to_fwrapper(self.input(), ('paper_id', 'doc'), rows)
 
 
-def build_paper_repdoc_corpus(
-        paper_vectors_file,
-        outfile='repdoc-by-paper-corpus.mm'):
-        filter_terms=True, no_below=2, no_above=1, feature_cap=None):
-    """Build a BoW representation of the paper repdoc corpus and save it in
-    Matrix Market (mm) format.
+class BuildPaperRepdocDictionary(luigi.Task):
+    """Build a dictionary to index the paper repdoc corpus."""
 
-    :param str paper_vectors_file: Name of file to read paper repdocs from; the
-        assumed format is (docid, repdoc_vector), where repdoc_vector is a
-        string of terms, with each term separated by '|' characters.
-    :param str outfile: Filename for .mm corpus output.
-    :param bool filter_terms: If True, filter out terms from the corpus.
-    :param int no_below: Terms are removed if they appear in less documents
-        than this (minimum number for remaining term frequencies).
-    :param float no_above: Terms are removed if they appear in more than this
-        proportion of documents.
-    :param feature_cap: Maximum number of terms to keep. If there are more than
-        this many terms, the least frequent are removed until this cap is
-        reached.
-    """
-    def read_repdocs(fname):
-        records = util.yield_csv_records(fname)
-        repdocs = (term_string.split('|') for _, term_string in records)
+    def requires(self):
+        return BuildPaperRepdocVectors()
 
-    # Build the dictionary of terms.
-    repdocs = read_repdocs(paper_vectors_file)
-    dictionary = gensim.corpora.Dictionary(repdocs)
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(config.data_dir, 'repdoc-by-paper-corpus.dict'))
 
-    # Filter out terms if requested.
-    if filter_terms:
+    def read_repdocs(self):
+        records = util.iter_csv_fwrapper(self.input())
+        return (doc.split('|') for _, doc in records)
+
+    def run(self):
+        repdocs = self.read_repdocs()
+        dictionary = gensim.corpora.Dictionary(repdocs)
         logging.info('term count in paper repdoc corpus pre-filtering: %d' %
                      len(dictionary))
-        feature_cap = len(dictionary) if feature_cap is None else feature_cap
-        dictionary.filter_extremes(no_below, no_above, feature_cap)
+        dictionary.filter_extremes(2, 1, len(dictionary))
         logging.info('term count in paper repdoc corpus post-filtering: %d' %
                      len(dictionary))
+        dictionary.save(self.output().path)
 
-    # Save the dictionary.
-    fname = '%s.dict' % os.path.splitext(outfile)[0]
-    dictionary.save(fname)
 
-    # Now build the MM corpus.
-    repdoc_corpus = read_repdocs(paper_vectors_file)
-    bow_corpus = (dictionary.doc2bow(doc) for doc in repdoc_corpus)
+class BuildPaperRepdocCorpus(luigi.Task):
+    """Build BoW representation and save it in MM format."""
 
-    # And save it to the specified output file.
-    if not outfile.endswith('.mm'):
-        outfile = '%s.mm' % os.path.splitext(outfile)[0]
-    gensim.corpora.MmCorpus.serialize(fname, bow_corpus)
+    def requires(self):
+        return [BuildPaperRepdocDictionary(),
+                BuildpaperRepdocVectors()]
+
+    def output(self):
+        return luigi.LocalTarget(
+                os.path.join(config.data_dir, 'repdoc-by-paper-corpus.mm'))
+
+    def run(self):
+        dict_file, vecs_file = self.input()
+        dictionary = gensim.corpora.Dictionary(dict_file.path)
+        records = util.iter_csv_fwrapper(vecs_file)
+        docs = ((docid, doc.decode('utf-8').split('|'))
+                for docid, doc in records)
+        bow_corpus = (dictionary.doc2bow(doc) for doc in repdoc_corpus)
+        gensim.corpora.MmCorpus.serialize(self.output().path, bow_corpus)
+
+
+class WritePaperToRepdocIdMap(luigi.Task):
+    """Map paper ids to contiguous mm document indices."""
+
+    def requires(self):
+        return BuildPaperRepdocVectors()
+
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(config.data_dir, 'paper-id-to-repdoc-id-map.csv'))
+
+    def run(self):
+        records = util.iter_csv_fwrapper(self.input())
+        rows = ((paper_id, docid)
+                for docid, (paper_id, _) in enumerate(records))
+        util.write_csv_to_fwrapper(self.output(), ('paper_id', 'doc_id'), rows)
 
 
 # ---------------------------------------------------------
@@ -148,3 +169,7 @@ def build_author_tfidf_corpus(bow_corpus_fname,
     tfidf = gensim.models.TfidfModel(bow_corpus)
     tfidf_corpus = tfidf[bow_corpus]
     gensim.corpora.MmCorpus.serialize(outfile, tfidf_corpus)
+
+
+if __name__ == "__main__":
+    luigi.run()
