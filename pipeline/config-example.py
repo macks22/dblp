1cd385bbc57f9e9b728913ac316486394c5cfb4c
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-02-11 16:24:26 -0500

    updated config example to include new dirs

--- pipeline/config-example.py
+++ pipeline/config-example.py
@@ -5,3 +5,6 @@ base_dir = '/data/username/aminer-network'
 data_dir = pjoin(base_dir, 'data')
 originals_dir = pjoin(base_dir, 'original-data')
 base_csv_dir = pjoin(data_dir, 'base-csv')
+filtered_dir = pjoin(data_dir, 'filtered-csv')
+repdoc_dir = pjoin(data_dir, 'repdocs')
+graph_dir = pjoin(data_dir, 'graphs')
