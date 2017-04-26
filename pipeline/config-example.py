0f7bfdaf96c785b983a64555fec94120a963b72a
Mack Sweeney <msweeney@researchinnovations.com>
2017-04-26 17:10:57 -0400

    fix confusing typo in config-example

--- pipeline/config-example.py
+++ pipeline/config-example.py
@@ -3,7 +3,7 @@ pjoin = os.path.join
 
 base_dir = '/data/username/aminer-network'
 data_dir = pjoin(base_dir, 'data')
-originals_dir = pjoin(base_dir, 'original-data')
+originals_dir = pjoin(data_dir, 'original-data')
 base_csv_dir = pjoin(data_dir, 'base-csv')
 filtered_dir = pjoin(data_dir, 'filtered-csv')
 repdoc_dir = pjoin(data_dir, 'repdocs')
